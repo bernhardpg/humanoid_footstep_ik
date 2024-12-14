1ac833388a183887d04fcf60cc9ddf9d482c4c4b
Bernhard Paus Graesdal <bernhardpg@gmail.com>
2024-12-13 22:54:07 -0500

    Print info on Atlas model

--- humanoid_footstep_ik/main.py
+++ humanoid_footstep_ik/main.py
@@ -10,6 +10,7 @@ from pydrake.geometry.all import MeshcatVisualizer
 from pydrake.math import RigidTransform, RollPitchYaw
 from pydrake.multibody.parsing import Parser
 from pydrake.multibody.plant import AddMultibodyPlantSceneGraph
+from pydrake.multibody.tree import JointIndex
 from pydrake.systems.analysis import Simulator
 from pydrake.systems.framework import DiagramBuilder
 from pydrake.visualization import AddDefaultVisualization, ModelVisualizer
@@ -31,11 +32,48 @@ if __name__ == "__main__":
     diagram = builder.Build()
     diagram.set_name("plant and scene_graph")
 
+    # Query the number of positions and velocities
+    num_positions = plant.num_positions()
+    num_velocities = plant.num_velocities()
+    total_states = num_positions + num_velocities
+
+    print(f"Number of positions: {num_positions}")
+    print(f"Number of velocities: {num_velocities}")
+    print(f"Total states: {total_states}")
+
+    # Get position and velocity names
+    position_names = []
+    velocity_names = []
+
+    for joint_index in range(plant.num_joints()):
+        joint = plant.get_joint(JointIndex(joint_index))
+        for position_index in range(joint.num_positions()):
+            pos_name = joint.name()
+            if joint.num_positions() > 1:
+                pos_name += f"_{position_index}"
+            position_names.append(pos_name)
+        for velocity_index in range(joint.num_velocities()):
+            vel_name = joint.name()
+            if joint.num_velocities() > 1:
+                vel_name += f"_{velocity_index}"
+            velocity_names.append(vel_name)
+
+    # Print position and velocity names
+    print("Positions:", position_names)
+    print("Velocities:", velocity_names)
+
     context = diagram.CreateDefaultContext()
+    plant_context = plant.GetMyMutableContextFromRoot(context)
+
+    default_positions = plant.GetPositions(plant_context)
+
+    new_positions = default_positions.copy()
+    new_positions[6] = 1.0  # set the z-position a bit higher
+    plant.SetPositions(plant_context, new_positions)
 
     simulator = Simulator(diagram, context)
     simulator.set_target_realtime_rate(1.0)
-    simulator.AdvanceTo(0.0)
+    simulator.AdvanceTo(0.05)
 
     while True:
         ...
